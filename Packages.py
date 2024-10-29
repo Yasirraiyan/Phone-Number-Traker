8ccf0769180f77c1d1c281c006ace19f30f45e0b
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-10-29 13:01:01 +0600

    Create Packages.py

---
+++ Packages.py
@@ -0,0 +1,5 @@
+# Importing the required modules from the phonenumbers library
+import phonenumbers
+from phonenumbers import timezone
+from phonenumbers import geocoder
+from phonenumbers import carrier
