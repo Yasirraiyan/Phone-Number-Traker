29b00bddd56b6472c23d04c6f9bc0265919ca7ad
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-10-29 13:07:27 +0600

    Create Result printer.py

---
+++ Result printer.py
@@ -0,0 +1,4 @@
+# Printing the results
+print("Time Zone for the number is: " + str(timeZone))
+print("Geo Location for the number is: " + geolocation)
+print("Service Provider for the number is: " + service)
